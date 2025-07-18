d09003e8c6b89c31e4bcbd1d41682f329f269fd1
BrendanChen <brendan9012@gmail.com>
2025-07-18 09:32:23 +0800

    千分號

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import App from './App.vue'
 import router from './router'
 import Loading from 'vue3-loading-overlay'
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
+import { currency } from './methods/filters'
 
 const app = createApp(App)
+app.config.globalProperties.$filters = {
+  currency,
+}
 app.use(VueAxios, axios)
 app.use(router)
 app.component('LoadingComponent', Loading)
