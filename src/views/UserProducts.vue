1d9b21557a031ca6da77a59dc90e80b8011baa46
BrendanChen <brendan9012@gmail.com>
2025-07-22 23:00:17 +0800

    產品列表第一版完成

--- src/views/UserProducts.vue
+++ src/views/UserProducts.vue
@@ -1,27 +1,43 @@
 <template>
-  <div class="products-banner">
-    <img src="https://picsum.photos/1200/300/?random=2" />
-  </div>
+  <div>
+    <div class="products-banner">
+      <img
+        src="https://demo.templatesjungle.com/wingtea/images/hero-image.jpg"
+        alt="商品列表banner"
+      />
+    </div>
 
-  <div class="product">
-    <div class="wrap">
-      <div class="item" v-for="item in products" :key="item.id">
-        <img alt="" :src="item.imageUrl" />
-        <p>{{ item.title }}</p>
-        <p>{{ $filters.currency(item.origin_price) }}</p>
-        <button>加入購物車</button>
+    <div class="product">
+      <div class="wrap">
+        <div class="item" v-for="item in products" :key="item.id">
+          <div class="img-box">
+            <img :src="item.imageUrl" alt="商品圖片" />
+            <button class="view-more-btn">查看更多</button>
+          </div>
+          <div class="content">
+            <p class="title">{{ item.title }}</p>
+            <p class="price">$NT{{ $filters.currency(item.origin_price) }}</p>
+            <button>加入購物車</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+  <PaginationVue :pages="pagination" @emit-pages="getProducts"></PaginationVue>
 </template>
 
 <script>
+import PaginationVue from '@/components/PaginationVue.vue'
+
 export default {
   data() {
     return {
       products: [],
       pagination: {},
     }
+  },
+  components: {
+    PaginationVue,
   },
   methods: {
     getProducts(page = 1) {
